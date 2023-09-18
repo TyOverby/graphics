fa8623f9d4f711ba487238798067544234e481a5
Ty Overby <ty@pre-alpha.com>
2023-09-17 22:04:56 -0400

    _

--- common/resize-canvas.js
+++ common/resize-canvas.js
@@ -20,9 +20,64 @@ function resizeCanvas(canvas, gl) {
   if (gl) {gl.viewport(0, 0, pixelsAcross, pixelsDown)}
 }
 
-export default function (canvas, gl){
+function legacy(canvas, gl){
     resizeCanvas(canvas, gl);
     window.addEventListener('resize', () => {
         resizeCanvas(canvas, gl);
     });
+}
+
+
+function resizeCanvasToDisplaySize(canvas, displayWidth, displayHeight) {
+
+  // Check if the canvas is not the same size.
+  const needResize = canvas.width  !== displayWidth ||
+                     canvas.height !== displayHeight;
+
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width  = displayWidth;
+    canvas.height = displayHeight;
+  }
+
+  return needResize;
+}
+
+function onResize(entries) {
+  for (const entry of entries) {
+    let width;
+    let height;
+    let dpr = window.devicePixelRatio;
+    if (entry.devicePixelContentBoxSize) {
+      // NOTE: Only this path gives the correct answer
+      // The other 2 paths are an imperfect fallback
+      // for browsers that don't provide anyway to do this
+      width = entry.devicePixelContentBoxSize[0].inlineSize;
+      height = entry.devicePixelContentBoxSize[0].blockSize;
+      dpr = 1; // it's already in width and height
+    } else if (entry.contentBoxSize) {
+      if (entry.contentBoxSize[0]) {
+        width = entry.contentBoxSize[0].inlineSize;
+        height = entry.contentBoxSize[0].blockSize;
+      } else {
+        // legacy
+        width = entry.contentBoxSize.inlineSize;
+        height = entry.contentBoxSize.blockSize;
+      }
+    } else {
+      // legacy
+      width = entry.contentRect.width;
+      height = entry.contentRect.height;
+    }
+    const displayWidth = Math.round(width * dpr);
+    const displayHeight = Math.round(height * dpr);
+    resizeCanvasToDisplaySize(entry.target, displayWidth, displayHeight);
+  }
+}
+
+
+
+export default function (canvas) {
+  const resizeObserver = new ResizeObserver(onResize);
+  resizeObserver.observe(canvas, {box: 'content-box'});  
 }
